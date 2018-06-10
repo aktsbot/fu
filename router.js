df6d2deb6d61eee57302ef0b8d45bc8d292cb22a
Ashish Kurian Thomas <box.ashishk@gmail.com>
2018-06-10 23:20:51 +0530

    added file serve endpoint

--- router.js
+++ router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const controller = require("./controller");
+const config = require("./config");
+const path = require("path");
 
 const router = express.Router();
 
 router.get("/health", controller.getHealth);
 router.post("/file", controller.uploadFile);
+router.use("/file", express.static(`${config.uploadLocation}`));
 
 module.exports = router;
